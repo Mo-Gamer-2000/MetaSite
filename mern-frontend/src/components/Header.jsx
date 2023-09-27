40521126621463f7f15f94d57c59b011040bd041
Moeez Abdul <moeezrajput2000@gmail.com>
2023-09-27 14:56:02 +0100

    Responsive Navbar Completed

--- mern-frontend/src/components/Header.jsx
+++ mern-frontend/src/components/Header.jsx
@@ -1,15 +1,23 @@
-// components/Navbar.js
-
-import React from "react";
+import React, { useState } from "react"; // <-- Import useState
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // <-- State to toggle the menu
+
   return (
     <nav>
       <div className="navbar-brand">
         <Link to="/">MetaSite</Link>
       </div>
-      <ul>
+      {/* Hamburger menu icon */}
+      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+        {menuOpen ? "✖" : "☰"}{" "}
+        {/* Conditionally render close or hamburger icon */}
+      </div>
+      <ul className={menuOpen ? "active" : ""}>
+        {" "}
+        {/* <-- Conditionally apply a class */}
         <li>
           <Link to="/">Home</Link>
         </li>
