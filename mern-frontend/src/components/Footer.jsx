ec26549ce0ee76754db98a8fddbdb89227ed591a
Moeez Abdul <moeezrajput2000@gmail.com>
2023-09-28 16:33:56 +0100

    Footer in Progress... .. .

--- mern-frontend/src/components/Footer.jsx
+++ mern-frontend/src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>All rights reserved by MetaSite</div>;
+  return (
+    <footer className="items-center shadow-inner">
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
+        <h1 className="lg:text-ellipsis">FOOTER</h1>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
