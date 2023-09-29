574aa02d12cd88ffcb42ca7eb9b3047d8c0accf7
Moeez Abdul <moeezrajput2000@gmail.com>
2023-09-29 22:54:32 +0100

    Copyright text added

--- mern-frontend/src/components/Footer.jsx
+++ mern-frontend/src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="items-center shadow-inner">
       <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#ffffff19] py-7">
-        <h1 className="lg:text-ellipsis">FOOTER</h1>
+        <h1 className="lg:text-ellipsis">
+          © {currentYear} MetaSite. All rights reserved.
+        </h1>
       </div>
     </footer>
   );
