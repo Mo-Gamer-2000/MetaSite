e8145230d46723f3dd568d05adfc01da57790936
Moeez Abdul <moeezrajput2000@gmail.com>
2023-10-12 14:32:53 +0100

    Basic Error Handler created

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 const cors = require("cors");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
-
 const app = express();
 const userRoutes = require("./routes/users");
+const errorHandler = require("./middlewares/errorHandler");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(limiter);
+app.use(errorHandler);
 
 // Routes
 app.use("/api/users", userRoutes);
