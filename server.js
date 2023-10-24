cb5b788a6e41f790ea364b8fc808220a8ce7c367
Moeez Abdul <moeezrajput2000@gmail.com>
2023-10-24 22:49:46 +0100

    Updated server.js, minot change - Added Comments

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
+// Importing necessary packages
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
-const cors = require("cors");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
+require("dotenv").config(); // Loading environment variables
+const cors = require("cors"); // Cross-Origin Resource Sharing
+const helmet = require("helmet"); // For setting HTTP headers for security
+const rateLimit = require("express-rate-limit"); // For API request rate limiting
+
+// Initializing express application
 const app = express();
+
+// Importing user-related routes
 const userRoutes = require("./routes/users");
+
+// Importing middleware to handle errors
 const errorHandler = require("./middlewares/errorHandler");
 
+// Setting up rate limiter to avoid abuse of the API
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
 
-// Middleware
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-app.use(limiter);
-app.use(errorHandler);
+// Middlewares
+app.use(express.json()); // Parsing incoming requests with JSON payloads
+app.use(cors()); // Enabling CORS for all routes
+app.use(helmet()); // Setting security headers
+app.use(limiter); // Using the rate limiter
+app.use(errorHandler); // Using the custom error handler
 
-// Routes
+// Setting up routes
 app.use("/api/users", userRoutes);
 const postRoutes = require("./routes/posts");
 app.use("/api/posts", postRoutes);
 
-// Connect to MongoDB
+// Connecting to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -34,14 +42,16 @@ mongoose
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch((err) => console.error(err));
 
+// Defining the port number
 const PORT = process.env.PORT || 5000;
 
-// Error handling
+// Generic error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something went wrong!");
 });
 
+// Starting the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
