279331376736ea912383479413e07c966730a0cb
Moeez Abdul <moeezrajput2000@gmail.com>
2023-11-13 11:11:49 +0000

    Working on CTA

--- mern-frontend/src/components/MainLayout.jsx
+++ mern-frontend/src/components/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import CTA from "./CTA/CTA";
 
 const mainLayout = ({ children }) => {
   return (
     <div>
       <Header />
       {children}
+      <CTA />
       <Footer />
     </div>
   );
